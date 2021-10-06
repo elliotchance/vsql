defc0e9046cd08d2309b61a6856856b5d66e9a67
Elliot Chance <elliotchance@gmail.com>
2021-10-05 20:42:34 -0400

    Fix windows binary (#63)
    Ths fixed the compilation of the windows binary because the locking code
    is only available on *nix systems.
    
    Unfortunately this wasn't caught on CI because we don't bother also
    building the windows binary. However, to protect against this happening
    in the future we now do.

---
+++ vsql/flock_nix.c.v
@@ -0,0 +1,27 @@
+// flock_nix.c.v contains file locking functions. flock() itself only protects
+// against concurrent access from different processes, so to protect against
+// goroutines we need to add an additional level of locking within this process.
+
+module vsql
+
+import os
+
+#include <fcntl.h>
+
+fn C.flock(int, int) int
+
+fn flock_lock_exclusive(file os.File, path string) ? {
+	C.flock(file.fd, C.LOCK_EX)
+}
+
+fn flock_lock_shared(file os.File, path string) ? {
+	C.flock(file.fd, C.LOCK_SH)
+}
+
+fn flock_unlock_exclusive(file os.File, path string) {
+	C.flock(file.fd, C.LOCK_UN)
+}
+
+fn flock_unlock_shared(file os.File, path string) {
+	C.flock(file.fd, C.LOCK_UN)
+}
